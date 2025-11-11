69575215c98d8df3d14b4fb19f4e495d656cdae1
Калашников Артём <a9032029915@gmail.com>
2025-11-11 11:31:01 +0300

    Выполнен ввод значения x.

--- Praktika06/Program.cs
+++ Praktika06/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значения параметра x = ");
+            string s = Console.ReadLine();
+            double x = Convert.ToDouble(s);
+        }
+    }
+}
